26f386658ab9c553522c202b8dbcd9486a732d3c
Max White <maxwell.oliver.white@gmail.com>
2019-10-13 21:52:16 -0700

    Add extra conversion unit tests.

--- server/map/coordinates/conversion.test.js
+++ server/map/coordinates/conversion.test.js
@@ -1,6 +1,11 @@
-const { pixelDensityToZoom } = require('./conversion');
+const {
+  degreesToRadians,
+  radiansToDegrees,
+  pixelDensityToZoom,
+  zoomToPixelSpace,
+} = require('./conversion');
 
-describe('zoom level', () => {
+describe('Zoom level ', () => {
   test('is never less than 0', () => {
     expect(pixelDensityToZoom(0)).toBe(0);
     expect(pixelDensityToZoom(1)).toBe(0);
@@ -17,7 +22,7 @@ describe('zoom level', () => {
     expect(pixelDensityToZoom(1024)).toBe(2);
   });
 
-  test('increases above powers of 2 ', () => {
+  test('increases above powers of 2', () => {
     expect(pixelDensityToZoom(256)).toBe(0);
     expect(pixelDensityToZoom(257)).toBe(1);
 
@@ -26,5 +31,30 @@ describe('zoom level', () => {
 
     expect(pixelDensityToZoom(1024)).toBe(2);
     expect(pixelDensityToZoom(1025)).toBe(3);
+  });
+});
+
+describe('Pixel space ', () => {
+  test('starts off at 256', () => {
+    expect(zoomToPixelSpace(0)).toBe(256);
+  });
+
+  test('doubles with each zoom level', () => {
+    expect(zoomToPixelSpace(1)).toBe(512);
+    expect(zoomToPixelSpace(2)).toBe(1024);
+  });
+});
+
+describe('Conversion is accurate from ', () => {
+  test('degrees to radians ', () => {
+    expect(degreesToRadians(0)).toBe(0);
+    expect(degreesToRadians(180)).toBe(Math.PI);
+    expect(degreesToRadians(360)).toBe(2*Math.PI);
+  });
+
+  test('radians to degrees', () => {
+    expect(radiansToDegrees(0)).toBe(0);
+    expect(radiansToDegrees(Math.PI)).toBe(180);
+    expect(radiansToDegrees(2 * Math.PI)).toBe(360);
   });
 });
