ad19a2355c6fd840688055bcefefb32bcb2d644f
Max White <maxwell.oliver.white@gmail.com>
2019-10-22 23:50:47 -0700

    Add boundaries page initial section styles.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
 const urlCreator = window.URL || window.webkitURL;
 
-const testLatLng = {
-  northWestLatLng: {lat: 37.4317565, lng: -122.1678751},
-  southEastLatLng: {lat: 37.2805374, lng: -121.9905719},
-}
-
-const hannaTokyo = {
-  northWestLatLng: {lat: 35.747398, lng: 139.5275113},
-  southEastLatLng: {lat:35.588414, lng:  139.894867},
-}
-
-const southPas = {
-  northWestLatLng: {lat: 34.1514208, lng:-118.1764449},
-  southEastLatLng: {lat:34.0960171, lng: -118.1158297},
-}
-
 function getInitialState() {
   return {
+    phase: 0,
+    bounds: false,
+    lock: false,
+    dimensions: { width: 1, height: 1 },
+    stretch: { x: 100, y: 100, z: 100 },
+    range: { x: false, y: false },
+
     img: false // stores image data
   };
 }
@@ -28,49 +20,75 @@ class App extends Component {
     this.state = {data: false};
 
     // Binding methods
-    this.setMap = this.setMap.bind(this);
+    //this.setMap = this.setMap.bind(this);
   }
 
   componentDidMount() {
-    return this.setMap()
+    // return this.setMap()
   }
 
-  setMap() {
-    console.log('get map in range: ', southPas);
-    const that = this; // loses reference to this when in post callback
-    fetch('/photo', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(southPas),
-    }).then(response => {
-      if (response.status >= 200 && response.status < 300) {
-        console.log(response)
-        response.blob().then(data => {
-          console.log(data);
-          that.setState(Object.assign(
-            that.state,
-            { img: data },
-          ));
-          return Promise.resolve();
-        })
-      }
-      return Promise.reject(response.statusText);
-    });
-  }
+  // setMap() {
+  //   console.log('get map in range: ', southPas);
+  //   const that = this; // loses reference to this when in post callback
+  //   fetch('/photo', {
+  //     method: 'POST',
+  //     headers: {
+  //       Accept: 'application/json',
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(southPas),
+  //   }).then(response => {
+  //     if (response.status >= 200 && response.status < 300) {
+  //       console.log(response)
+  //       response.blob().then(data => {
+  //         console.log(data);
+  //         that.setState(Object.assign(
+  //           that.state,
+  //           { img: data },
+  //         ));
+  //         return Promise.resolve();
+  //       })
+  //     }
+  //     return Promise.reject(response.statusText);
+  //   });
+  // }
 
   render() {
     console.log("App page rendering...")
 
-    if (this.state.img) document.getElementById('mypic').src = urlCreator.createObjectURL(this.state.img);
+    // Unnecessary for now
+    // if (this.state.img) document.getElementById('mypic').src = urlCreator.createObjectURL(this.state.img);
+    // const myblock = (
+    //   <div id="myblock"></div>
+    // );
+    // const g = document.createElement('div');
+    // g.setAttribute("id", "myblock");
+    // const map = new google.maps.Map(myblock, {
+    //   center: {lat: -34.397, lng: 150.644},
+    //   zoom: 8,
+    //   clickableIcons: false,
+    //   fullscreenControl: false,
+    //   mapTypeControl: true,
+    //   mapTypeControlOptions: {
+    //     mapTypeIds: ["hybrid", "roadmap"]
+    //   },
+    //   rotateControl: false,
+    //   streetViewControl: false,
+    //   tilt: 0
+    // });
 
     return (
-      <div>
-        <h1>Hello!</h1>
+      <div id="boundsContainer">
+        <div id="boundsArea"><div id="myblock"></div></div>
+        <div id="boundsOptions"></div>
       </div>
     );
+
+    // return (
+    //   <div>
+    //     <h1>Hello!</h1>
+    //   </div>
+    // );
   }
 }
 
