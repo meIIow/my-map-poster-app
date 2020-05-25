20293e9857762d89e513ba9fc4fe012bd1be5ee5
Max White <maxwell.oliver.white@gmail.com>
2020-05-24 22:49:46 -0700

    Clean up old size option code.

--- client/components/SelectSize.jsx
+++ client/components/SelectSize.jsx
@@ -46,29 +46,4 @@ const SelectSize = (props) => {
   );
 }
 
-// const SelectSize = (props) => {
-//   return (
-//     <div>
-//       <Instructions stepNumber={props.phase} stepName={STEP_NAME} instructions={INSTRUCTIONS}/>
-//       <div class="tab">
-//         <button class={`tablinks${props.withUnits ? ' active' : ''}`} onClick={() => props.setWithUnits(true)}>Units & Resolution</button>
-//         <button class={`tablinks${!props.withUnits ? ' active' : ''}`} onClick={() => props.setWithUnits(false)}>Pixel Dimensions</button>
-//       </div>
-//       <div>
-//         <div class="halfsies">width units:
-//           <input type="number" disabled={!props.withUnits} class="units" id="width-units" min="0" onInput={(e)=> props.setUnits(e.target.value, null)} value={(props.lock) ? props.ratio.width * props.mult.ratio : props.dimensions.x * props.mult.dimensions / props.resolution}></input>
-//         </div>
-//         <div class="halfsies">height units:
-//           <input type="number" disabled={!props.withUnits} class="units" id="height-units" min="0" onInput={(e)=> props.setUnits(null, e.target.value)} value={(props.lock) ? props.ratio.height * props.mult.ratio : props.dimensions.y * props.mult.dimensions / props.resolution}></input>
-//         </div>
-//         <div>Pixel Density:<input type="number" disabled={!props.withUnits} value={props.resolution} min="0" step="50" onInput={(e)=> props.updateResolution(e.target.value)}></input></div>
-//       </div>
-//       <div>width-pixels:</div>
-//       <input type="number" disabled={props.withUnits} id="width-pixels" min="1" value={Math.round((props.lock) ? props.ratio.width * props.mult.ratio  * props.resolution : props.dimensions.x * props.mult.dimensions)} onInput={(e)=> props.updatePixels(e.target.value, null)}></input>
-//       <div>height-pixels:</div>
-//       <input type="number" disabled={props.withUnits} id="height-pixels" min="1" value={Math.round((props.lock) ? props.ratio.height * props.mult.ratio * props.resolution : props.dimensions.y * props.mult.dimensions)} onInput={(e)=> props.updatePixels(null, e.target.value)}></input>
-//     </div>
-//   );
-// }
-
 export default SelectSize;
